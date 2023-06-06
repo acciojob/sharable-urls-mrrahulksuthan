2e3c81b5fdafe9e1bdb10a4bf741d3f83c41237d
mrrahulksuthan <86397472+mrrahulksuthan@users.noreply.github.com>
2023-06-06 20:38:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // your code here
+document.getElementById("myForm").addEventListener("submit", function(event) {
+      event.preventDefault(); // Prevent form submission
+
+      var name = document.getElementById("name").value;
+      var year = document.getElementById("year").value;
+
+      var queryString = "?name=" + encodeURIComponent(name) + "&year=" + encodeURIComponent(year);
+
+      document.getElementById("url").textContent = "https://localhost:8080/" + queryString;
+    });
